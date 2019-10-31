b44b61499983b9e6933b6df6a3344921fbc0c42e
Jan Campens <jan.campens@gmail.com>
2019-10-31 23:55:01 +0100

    Update root module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ScenesComponent } from './scenes/scenes.component';
+import { SceneDetailComponent } from './scene-detail/scene-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ScenesComponent,
+    SceneDetailComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
