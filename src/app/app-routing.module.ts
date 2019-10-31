f493d4927452426e0df3dc7382b30060ec7ab9fb
Jan Campens <jan.campens@gmail.com>
2019-11-01 00:34:10 +0100

    Add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { ScenesComponent } from './scenes/scenes.component';
+import { SceneDetailComponent } from './scene-detail/scene-detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'overview', pathMatch: 'full'},
+  { path: 'overview', component: ScenesComponent},
+  { path: 'detail/:id', component: SceneDetailComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
