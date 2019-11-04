3e1410e7d05824287d0acb55a0a02d517ed7439f
Jan Campens <jan.campens@gmail.com>
2019-11-04 19:38:08 +0100

    Remove toolbar

--- src/app/material-module.ts
+++ src/app/material-module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 
 import { MatListModule } from '@angular/material/list';
-import { MatToolbarModule } from '@angular/material/toolbar';
 import {MatPaginatorModule} from '@angular/material/paginator';
 
 @NgModule({
     exports: [
         MatListModule,
-        MatToolbarModule,
         MatPaginatorModule,
     ]
 })
